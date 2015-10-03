58a7d43bbee04d16e75f5078ed05ba8a76a42ee9
Stefano Baghino <stefano@baghino.me>
2015-10-03 15:54:23 +0200

    moved initialization to base class

--- src/main/scala/me/baghino/spark/intro/TwitterExample.scala
+++ src/main/scala/me/baghino/spark/intro/TwitterExample.scala
@@ -9,29 +9,7 @@ import twitter4j.Status
 
 object TwitterExample extends App with TwitterExampleBase {
 
-  // --------------------
-  // ## 1. INITIALIZATION
-
-  // First, let's configure Spark
-  // We will deploy locally using a thread for each core (that's what the '*' stands for)
-  val sparkConfiguration = new SparkConf().
-    setAppName("spark-twitter-stream-example").
-    setMaster("local[*]")
-
-  // Let's create the Spark Context using the configuration we just created
-  val sparkContext = new SparkContext(sparkConfiguration)
-
-  // Now let's wrap the context in a streaming one, passing along the window size
-  val streamingContext = new StreamingContext(sparkContext, Seconds(5))
-
-  // Creating a stream from Twitter (see the README to learn how to configure it)
-  val tweets: DStream[Status] = 
-    TwitterUtils.createStream(streamingContext, None)
-
-  // ----------------
-  // ## 2. PROCESSING
-
-  // All processing functions are defined in the TwitterExampleBase class
+  // Initialization and processing functions are defined in the TwitterExampleBase class
 
   // Let's extract the words of each tweet
   // We'll carry the tweet along in order to print it in the end
@@ -57,10 +35,7 @@ object TwitterExample extends App with TwitterExampleBase {
   // Transform the (tweet, score) tuple into a readable string and print it
   textAndNonNeutralScore.map(makeReadable).print
 
-  // -------------------------
-  // ## 3. TURN ON THE ENGINE!
-
-  // Start the streaming
+  // Now that the streaming is defined, start it
   streamingContext.start()
 
   // Let's await the stream to end - forever
--- src/main/scala/me/baghino/spark/intro/TwitterExampleBase.scala
+++ src/main/scala/me/baghino/spark/intro/TwitterExampleBase.scala
@@ -1,5 +1,9 @@
 package me.baghino.spark.intro
 
+import org.apache.spark.streaming.dstream.DStream
+import org.apache.spark.streaming.twitter.TwitterUtils
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.{SparkContext, SparkConf}
 import twitter4j.Status
 
 import scala.io.Source
@@ -11,32 +15,48 @@ trait TwitterExampleBase {
   type TweetText = String
   type Sentence = Seq[String]
 
+  // First, let's configure Spark
+  // We will deploy locally using a thread for each core (that's what the '*' stands for)
+  val sparkConfiguration = new SparkConf().
+    setAppName("spark-twitter-stream-example").
+    setMaster("local[*]")
+
+  // Let's create the Spark Context using the configuration we just created
+  val sparkContext = new SparkContext(sparkConfiguration)
+
+  // Now let's wrap the context in a streaming one, passing along the window size
+  val streamingContext = new StreamingContext(sparkContext, Seconds(5))
+
+  // Creating a stream from Twitter (see the README to learn how to configure it)
+  val tweets: DStream[Status] =
+    TwitterUtils.createStream(streamingContext, None)
+
   // FIXME There's a smarter way to do this: see the "broadcast-var" branch
-  private val uselessWords = Source.fromFile("src/main/resources/stop-words.dat").getLines().toList
-  private val positiveWords = Source.fromFile("src/main/resources/pos-words.dat").getLines().toList
-  private val negativeWords = Source.fromFile("src/main/resources/neg-words.dat").getLines().toList
+  val uselessWords = Source.fromFile("src/main/resources/stop-words.dat").getLines().toList
+  val positiveWords = Source.fromFile("src/main/resources/pos-words.dat").getLines().toList
+  val negativeWords = Source.fromFile("src/main/resources/neg-words.dat").getLines().toList
   
-  protected def wordsOf(tweet: TweetText): Sentence =
+  def wordsOf(tweet: TweetText): Sentence =
     tweet.split(" ")
   
-  protected def toLowercase(sentence: Sentence): Sentence =
+  def toLowercase(sentence: Sentence): Sentence =
     sentence.map(_.toLowerCase)
   
-  protected def keepActualWords(sentence: Sentence): Sentence =
+  def keepActualWords(sentence: Sentence): Sentence =
     sentence.filter(_.matches("[a-z]+"))
   
-  protected def keepMeaningfulWords(sentence: Sentence): Sentence =
+  def keepMeaningfulWords(sentence: Sentence): Sentence =
     sentence.filter(!uselessWords.contains(_))
 
-  protected def extractWords(sentence: Sentence): Sentence =
+  def extractWords(sentence: Sentence): Sentence =
     sentence.map(_.toLowerCase).filter(_.matches("[a-z]+"))
 
-  protected def computeWordScore(word: String): Int =
+  def computeWordScore(word: String): Int =
     if (positiveWords.contains(word))       1
     else if (negativeWords.contains(word)) -1
     else                                    0
 
-  protected def computeSentenceScore(words: Sentence): Int =
+  def computeSentenceScore(words: Sentence): Int =
     words.map(computeWordScore).sum
 
 }
